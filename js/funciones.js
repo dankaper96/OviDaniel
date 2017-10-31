0aa0c4bf001e97982a111fe6bd8cd62c440afc35
dankaper96 <dmunozbuitrago@gmail.com>
2017-10-30 19:20:20 -0500

    coimite actividades
    act actividades

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+/*-----------------------------------------------------------------------------------
+	Nombre: DISEÑOS DE SITIOS WEB | Fase de Construcción
+	URI: 
+	Descripcion: Unidad 1 y 2: Fase de Construcción - Crear un OVI en formato WEB con HTML5 y CSS3.
+	Autores: Jhonattan Guillermo Rodriguez Rojas - 
+-----------------------------------------------------------------------------------*/
